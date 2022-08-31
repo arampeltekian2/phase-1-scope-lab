1c73af52a9ac423c096c2b1a54d3b9592797a639
Aram <arampeltekian2@gmail.com>
2022-08-31 11:51:47 -0500

    commit tree 1baefad6a76764695568634f67750e8fa5dd71fd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob'; 
+const leastFavoriteCustomer= 'not bob';
+function newCustomerName(bob){
+}
+function upperCaseCustomerName(){
+    customerName= 'BOB'
+}
+function setBestCustomer(){
+    bestCustomer= 'not bob'  
+
+}
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'bob'
+}
